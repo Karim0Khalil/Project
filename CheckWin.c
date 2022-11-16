c02e8bed3dfdd6cf19ddef9108e56a92a8f3f88a
Mohammad Chehab <mgc10@mail.aub.edu>
2022-11-17 00:34:50 +0200

    Added specifications and test cases for most of the functions

--- CheckWin.c
+++ CheckWin.c
@@ -183,12 +183,51 @@ int isOver(int **array, int column)
     else if (result > 0 && input == 2)
         return -1;
 }
-// int nextopenRow(int **board, int column)
-// {
-// 	for (int i = 0; i < 6; i++)
-// 		if (board[i][column] == 0)
-// 			return i;
-// }
+/*Requires: An int** board argument(2D array) , and an integer argument which is a column(values allowed btwn 0-6)
+Effects: Returns the first valid row where the player can place the peice at the given column
+Testing: 2 Sufficeint test cases:
+1-
+ Given a board allocated dynamically with malloc, given the column where we need to check at what row position can we place:
+example of board: consider: {
+	 {2,1,1,2,2,1,0}
+	,{2,1,1,2,2,1,2}
+	,{1,2,2,1,1,2,1}
+	,{2,1,2,2,1,1,2}
+	,{1,2,2,1,1,2,1},
+	 {2,1,1,2,2,1,2}
+	 };
+	nextopenRow(board,6) will output 0;
+2-Given an empty array we must get row 0, since no player has played yet
+example: 
+	 {0,0,0,0,0,0,0}
+	,{0,0,0,0,0,0,0}
+	,{0,0,0,0,0,0,0}
+	,{0,0,0,0,0,0,0}
+	,{0,0,0,0,0,0,0},
+	 {0,0,0,0,0,0,0}
+	 };
+	 nextopenRow(board,6) will also output 0.
+*/
+int nextopenRow(int **board, int column)
+{
+	for (int i = 0; i < 6; i++)
+		if (board[i][column] == 0)
+			return i;
+}
+/*Requires: The 2D array board with 6 rows and 7 columns
+Effects: Returns a 1D array with the valid locations(meaning the column is valid to make 
+a move in) of the numbered columns in the board, -1 otherwise. 
+*/
+/*Test cases: 1- If given a valid input the functions performs its operations correctly.
+given an 2Darray(gameboard)
+int gameboard[6][7] = {{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,2,0,0,0},{0,1,1,1,2,2,0}};
+The functions returns a 1D array with the number of the columns that are valid to make a move in.(columns are 0 indexed).
+Output: {0,1,2,3,4,5,6};
+2- int gameboard[6][7] = {{1,0,0,0,0,0,0},{2,0,0,0,0,0,0},{1,0,0,0,0,0,0},{2,0,0,0,0,0,0},{1,0,0,2,0,0,0},{2,1,1,1,2,2,0}};
+Output: {1,0,0,0,0,0,0}
+This function will always output the correct result since we are always giving it a fixed sized board with valid arguments
+in our function calls.
+*/
 int *GetValidLocations(int **board)
 {
     int *arr = (int *)malloc(sizeof(int) * 7);
@@ -203,6 +242,14 @@ int *GetValidLocations(int **board)
     }
     return arr;
 }
+/*Requires: A 1D array of size 4, size of the given 1D array(as an integer) which is 4, and an integer named piece which can
+have the values 1 or 2.
+Effects:Returns the number of times piece is found in the window array
+Simple Testing case:
+window = {1,2,1,2};
+CountWindow(window,4,2) will ouput 2. 
+The algorithm is clear and is always guranteed to output the correct answer.
+*/
 int CountWindow(int window[], int size, int piece)
 {
     int count = 0;
@@ -215,6 +262,27 @@ int CountWindow(int window[], int size, int piece)
     }
     return count;
 }
+/*Requires: A 1D array of size 4, size of the given 1D array(as an integer) which is 4, and an integer named piece which can
+have the values 1 or 2.
+Effects: Returns a value which is named as score which is used to in other functions namely the minimax function(check minimax function
+for details.)This value evaluates the given window based on the elements contained in the window.
+Testing Cases: Given a window of size 4:
+1-window{1,1,1,1}
+ evauluate_window(int window[], 4, 1) will output score 100 which is correct.
+2-window{1,1,0,1};
+ evauluate_window(int window[], 4, 1) will output score 5 which is correct.
+ 3-window{1,1,0,0}
+ evauluate_window(int window[], 4, 1) will output score 2 which is correct.
+ 4-window{2,0,2,2}
+evauluate_window(int window[], 4, 1) will output score -4 which is correct.
+
+If peices 1 and 2 are found in the array, the score won't be updated.
+
+ Permutations of the list won't effect the algorithm, since we assume that the CountWindow function is implemented correclty and tested and
+ it is used in our evaluating window function.
+These are the 4 cases that can be experimented, otherwise if for example we enter a negative number, the function will eventaully return
+0 score. Enterting wrong arguments is the fault of the client, since we specified in the precondition that the piece entered must be 1 or 2.
+*/
 int evauluate_window(int window[], int size, int piece)
 {
     int score = 0;
@@ -241,7 +309,20 @@ int evauluate_window(int window[], int size, int piece)
     }
     return score;
 }
-
+/*Requires: A 2D array board of 6 rows and 7 columns. 
+Effects: Returns a double pointer which refers to the address of a copy of the given board which is a 2D array.
+Testing: given the board[6][7] by:
+	{
+	 {2,1,1,2,2,1,0}
+	,{2,1,1,2,2,1,2}
+	,{1,2,2,1,1,2,1}
+	,{2,1,2,2,1,1,2}
+	,{1,2,2,1,1,2,1},
+	 {2,1,1,2,2,1,2}
+	 };
+	 int **board = copyFunction(gameboard);
+	 The newly created board will be a 2D array a copy of gameboard(having a different address).
+*/
 int **copyFunction(int **board)
 {
     int **copy =(int**) malloc(6 * sizeof(int *));
@@ -268,7 +349,47 @@ int **copyFunction(int **board)
     }
     return copy;
 }
-
+/*Requires: A 2D array which is the board of 6 rows and 7 columns and an integer piece argument piece which can have the value of 1 or 2
+Effects: Returns a given score , using the evaluate window functions for rows,columns, left and right diagonals. 
+Testing Cases: The function int scoreposition(int board[6][7], int piece) uses the help of the function evaluate position which we assume
+it is correct. 
+The function will give us the combinations of all the rows and columns, and diagonals in a 1D array.
+Note: we tested each function call seperately and it resulted in outputing correct answers.
+1-For even entries: 
+scorepostion(gameboard,1); && scorepostion(gameboard,2);
+ int gameboard[6][7] = {
+	 {0,1,1,2,2,1,0}
+	,{2,1,1,2,2,1,2}
+	,{1,2,2,1,1,2,1}
+	,{2,1,2,2,1,1,2}
+	,{1,2,2,1,1,2,1},
+	 {2,1,1,2,2,1,2}
+	 };
+ gave us the correct output.
+ 2-For odd entries:
+ scorepostion(gameboard,1) && scorepostion(gameboard,2);
+	 int gameboard[6][7] = {
+	 {2,1,1,2,2,1,0}
+	,{2,1,1,2,2,1,2}
+	,{1,2,2,1,1,2,1}
+	,{2,1,2,2,1,1,2}
+	,{1,2,2,1,1,2,1},
+	 {2,1,1,2,2,1,2}
+	 };
+	gave us the correct output.
+3- For 42 entries(meaning the board is empty)
+	scorepostion(gameboard,1) && scorepostion(gameboard,2);
+	int gameboard[6][7]=
+	 {0,0,0,0,0,0,0}
+	,{0,0,0,0,0,0,0}
+	,{0,0,0,0,0,0,0}
+	,{0,0,0,0,0,0,0}
+	,{0,0,0,0,0,0,0},
+	 {0,0,0,0,0,0,0}
+	 };
+	 gave us the correct result here the result for the pieces 1 or 2.
+	 scorepostions for 1 and 2 here gives us the same result , since the board is empty.
+*/
 int scoreposition(int **board, int piece)
 {
     int score = 0;
@@ -345,7 +466,18 @@ int scoreposition(int **board, int piece)
     }
 
     return score;
-}
+}/*Requires: An array pointer
+Effects: Returns an integer namely size which counts the number of valid positions(if index at i !=-1)
+Testing cases:
+1-array with all zero's will return 0, since no valid positions initially.
+2-array with -1's will output the correct output which is the number of -1 found in the array.
+Manual Example:
+int *Valid = int*(malloc(sizeof(int))7);
+for(int i=0;i<7;++i)
+Valid[i]=-1;
+NumberOfValidPositions(Valid) will output 7.
+All other cases are trivial...
+*/
 int NumberOfValidPositions(int *ValidPositions)
 {
     int size=0;
